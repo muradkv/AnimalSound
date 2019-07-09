2bfbe3b6824c3fd83fc4e82a8fbecb4da67dfde6
murad <muradkv@gmail.com>
2019-07-09 23:10:29 +0300

    Play sound when user tapped on animal emoji

--- AnimalSound/ViewController.swift
+++ AnimalSound/ViewController.swift
@@ -11,18 +11,28 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet var animalSoundLabel: UILabel!
     
+    let meowSound = SimpleSound(named: "meow")
+    let woofSound = SimpleSound(named: "woof")
+    let mooSound = SimpleSound(named: "moo")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     @IBAction func catButtonTapped(_ sender: UIButton) {
+        animalSoundLabel.text = "Meow!"
+        meowSound.play()
     }
     
     @IBAction func dogButtonTapped(_ sender: UIButton) {
+        animalSoundLabel.text = "Woof!"
+        woofSound.play()
     }
     
     @IBAction func cowButtonTapped(_ sender: UIButton) {
+        animalSoundLabel.text = "Mooo!"
+        mooSound.play()
     }
 }
 
