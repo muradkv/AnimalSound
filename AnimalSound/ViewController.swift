aa999ac1202ebf4acb42db60e2d083469465df31
murad <muradkv@gmail.com>
2019-07-09 22:54:27 +0300

    Add UI: buttons with animals and two label

--- AnimalSound/ViewController.swift
+++ AnimalSound/ViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var animalSoundLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func catButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func dogButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func cowButtonTapped(_ sender: UIButton) {
+    }
 }
 
